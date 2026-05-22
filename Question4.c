10c42b232f4f956c61542aeef17fc0b9fcad3b2b
arvindshuklaa <arvindshukla895752@gmail.com>
2026-05-22 23:50:09 +0530

    Add files via upload
    Day - 1

---
+++ Question4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int n , count = 0;
+    printf("Enter Number : ");
+    scanf("%d" ,&n);
+
+    if(n==0){
+        count = 1;
+    }
+    while(n != 0){
+        n = n / 10;
+        count++ ;
+    }
+    printf("Number of digits = %d" ,count);
+    return 0;
+}
