a8cfb899943c0c4ad3d98912dc137241325c7ce0
arvindshuklaa <arvindshukla895752@gmail.com>
2026-06-21 14:37:25 +0530

    Added Day 21 Programs

---
+++ Day 21/Question2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string : ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[length] != '\0' && str[length] != '\n'){
+        length++;
+    }
+    printf("Reversed String : ");
+
+    for(int i = length - 1; i >= 0; i--){
+        printf("%c", str[i]);
+    }
+    printf("\n");
+    return 0;
+}
